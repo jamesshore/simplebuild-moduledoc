59757311fa7d7018841d2f069493906820d79a7a
James Shore <jshore@jamesshore.com>
2014-01-18 22:46:44 -0800

    Started testing interpolator (for creating .md files)

---
+++ src/_interpolator_test.js
@@ -0,0 +1,14 @@
+/* Copyright (c) 2014 James Shore - See README file for license */
+"use strict";
+
+var expect = require("expect.js");
+
+var interpolator = require("./interpolator.js");
+
+describe("Interpolator", function() {
+
+	it("should run tests", function() {
+
+	});
+
+});

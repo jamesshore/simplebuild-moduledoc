1590e641677107043c190b1b77e31427ca334679
James Shore <jshore@jamesshore.com>
2014-01-18 22:53:05 -0800

    Replaced expect.js with chai

--- src/_interpolator_test.js
+++ src/_interpolator_test.js
@@ -1,14 +1,13 @@
 /* Copyright (c) 2014 James Shore - See README file for license */
 "use strict";
 
-var expect = require("expect.js");
+var expect = require("chai").expect;
 
 var interpolator = require("./interpolator.js");
 
 describe("Interpolator", function() {
 
 	it("should run tests", function() {
-
 	});
 
 });

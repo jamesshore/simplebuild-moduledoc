6e1285ac2a67645ac6c834da44cf5e7b7beca537
James Shore <jshore@jamesshore.com>
2014-01-19 23:17:41 -0800

    Started on converting descriptors to Handlebars contexts

---
+++ src/_analyze_test.js
@@ -0,0 +1,37 @@
+// Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+"use strict";
+
+var expect = require("chai").expect;
+var analyze = require("./analyze.js");
+var messages = require("./messages.js");
+
+describe("Descriptor", function() {
+
+	describe("transformModules", function() {
+
+		it("transforms all tasks", function() {
+			var module = {
+				foo: function() {},
+				bar: function() {},
+				baz: function() {}
+			};
+
+			expect(analyze.transformModule(module)).to.deep.equal({
+				task: [
+					analyze.transformTask(module, "foo"),
+					analyze.transformTask(module, "bar"),
+					analyze.transformTask(module, "baz")
+				]
+			});
+		});
+
+		it("throws exception if module is empty", function() {
+			var module = {};
+
+			expect(function() {
+				analyze.transformModule(module);
+			}).to.throw(Error, messages.EMPTY_MODULE);
+		});
+
+	});
+});
---
+++ src/analyze.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+"use strict";
+
+var messages = require("./messages.js");
+
+exports.transformModule = function(module) {
+	var tasks = Object.keys(module);
+	if (tasks.length === 0) throw new Error(messages.EMPTY_MODULE);
+
+	return {
+		task: tasks.map(exports.transformTask.bind(null, module))
+	};
+};
+
+exports.transformTask = function(module, key) {
+	return {
+		name: key
+	};
+};
